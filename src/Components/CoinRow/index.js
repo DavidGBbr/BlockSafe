c0c5bf811ffc9734c3b88ffc426edb503d461f43
davidGBbr <davidgomesbrigido@gmail.com>
2023-05-05 19:30:07 -0300

    BS - #005

--- src/Components/CoinRow/index.js
+++ src/Components/CoinRow/index.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import * as C from "./styles";
 import { Text } from "react-native";
+import axios from "axios";
 
 export const CoinRow = ({ coin }) => {
+  const [coinName, setCoinName] = useState();
+
+  const getCoin = async (id) => {
+    const response = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/${id}`
+    );
+    setCoinName(response.data);
+    console.log(response.data);
+  };
+
   return (
-    <C.TouchableOpacity>
+    <C.TouchableOpacity onPress={() => getCoin(coin.id)}>
       <C.Image source={{ uri: coin.image }} />
       <C.CoinInfo>
         <C.CoinName>{coin.name}</C.CoinName>
