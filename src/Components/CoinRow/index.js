e66179e40828a7be07b7837a7b334fa79ef9f350
davidGBbr <davidgomesbrigido@gmail.com>
2023-05-06 21:03:40 -0300

    BS - #006

--- src/Components/CoinRow/index.js
+++ src/Components/CoinRow/index.js
@@ -1,31 +1,40 @@
 import { useState } from "react";
 import * as C from "./styles";
-import { Text } from "react-native";
-import axios from "axios";
+import { Modal, View } from "react-native";
+import { ModalInfo } from "../ModalInfo";
 
 export const CoinRow = ({ coin }) => {
   const [coinName, setCoinName] = useState();
+  const [modalActive, setModalActive] = useState(false);
 
   const getCoin = async (id) => {
-    const response = await axios.get(
-      `https://api.coingecko.com/api/v3/coins/${id}`
-    );
-    setCoinName(response.data);
-    console.log(response.data);
+    setModalActive(true);
+    setCoinName(id);
+    console.log(`Criptomoeda: ${id}`);
   };
 
   return (
-    <C.TouchableOpacity onPress={() => getCoin(coin.id)}>
-      <C.Image source={{ uri: coin.image }} />
-      <C.CoinInfo>
-        <C.CoinName>{coin.name}</C.CoinName>
-        <C.Price>
-          {coin.current_price.toLocaleString("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-          })}
-        </C.Price>
-      </C.CoinInfo>
-    </C.TouchableOpacity>
+    <View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalActive}
+        onRequestClose={() => setModalActive(false)}
+      >
+        <ModalInfo setModalActive={setModalActive} coinName={coinName} />
+      </Modal>
+      <C.TouchableOpacity onPress={() => getCoin(coin.id)}>
+        <C.Image source={{ uri: coin.image }} />
+        <C.CoinInfo>
+          <C.CoinName>{coin.name}</C.CoinName>
+          <C.Price>
+            {coin.current_price.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          </C.Price>
+        </C.CoinInfo>
+      </C.TouchableOpacity>
+    </View>
   );
 };
