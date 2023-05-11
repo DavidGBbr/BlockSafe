f81fe8cdd80ac073166db1f4bec16a5ae2e698b3
davidGBbr <davidgomesbrigido@gmail.com>
2023-05-11 18:45:02 -0300

    BS -#014

--- src/Components/CoinGraphic/index.js
+++ src/Components/CoinGraphic/index.js
@@ -1,13 +1,89 @@
+import moment from "moment/moment";
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Dimensions } from "react-native";
+import { LineChart } from "react-native-chart-kit";
+import * as C from "./styles";
 
 export const CoinGraphic = ({ data, sparkline }) => {
-  // console.log(data);
+  const times = sparkline.map((item) => item.x);
+  const prices = sparkline.map((item) => item.y);
+
+  const dataLabels = times.reduce((acc, timestamp) => {
+    const date = moment.unix(timestamp);
+    const formattedDate = `Dia ${date.format("D")}`;
+    if (!acc.includes(formattedDate)) {
+      acc.push(formattedDate);
+    }
+    return acc;
+  }, []);
+
+  const reducedPrices = prices.reduce((acc, price, index) => {
+    const timestamp = times[index];
+    const date = moment.unix(timestamp);
+    const formattedDate = `Dia ${date.format("D")}`;
+    if (!acc[formattedDate]) {
+      acc[formattedDate] = {
+        value: 0,
+        count: 0,
+      };
+    }
+    acc[formattedDate].value += price;
+    acc[formattedDate].count++;
+    return acc;
+  }, {});
+
+  const chartData = {
+    labels: dataLabels.slice(-7),
+    datasets: [
+      {
+        data: dataLabels
+          .slice(-7)
+          .map((label) =>
+            reducedPrices[label]
+              ? reducedPrices[label].value / reducedPrices[label].count
+              : 0
+          ),
+      },
+    ],
+  };
+
+  const chartConfig = {
+    backgroundGradientFrom: "#1c1c1c",
+    backgroundGradientTo: "#1c1c1c",
+    color: (opacity = 1) => `rgba(228, 193, 49, ${opacity})`,
+    strokeWidth: 3,
+    useShadowColorFromDataset: true,
+    propsForDots: {
+      r: "3",
+      strokeWidth: "2",
+      stroke: "#ffa726",
+    },
+    propsForLabels: {
+      fontSize: 14,
+      fontWeight: "bold",
+      fill: "#fff",
+    },
+    propsForVerticalLabels: {
+      fontSize: 14,
+      fontWeight: "bold",
+      fill: "#fff",
+    },
+  };
+
   return (
-    <View>
+    <C.Container>
       <Text style={{ fontSize: 26, color: "#fff" }}>
-        Gráfico da Criptomoeda
+        Gráfico em USD {data.symbol}
       </Text>
-    </View>
+
+      <View>
+        <LineChart
+          data={chartData}
+          width={Dimensions.get("window").width - 40}
+          height={220}
+          chartConfig={chartConfig}
+        />
+      </View>
+    </C.Container>
   );
 };
---
+++ src/Components/CoinGraphic/styles.js
@@ -0,0 +1,52 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  gap: 20px;
+`;
+
+export const TitlesWrapper = styled.View`
+  margin: 0px 16px;
+`;
+
+export const UpperTitles = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const UpperLeftTitle = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Image = styled.Image`
+  width: 24px;
+  height: 24px;
+  margin-right: 4px;
+`;
+
+export const Subtitle = styled.Text`
+  font-size: 14px;
+  color: #a9abb1;
+`;
+
+export const LowerTitles = styled.Text`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const BoldTitle = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+export const Title = styled.Text`
+  font-size: 18px;
+`;
+
+export const ContainerChart = styled.View`
+  margin-top: 40px;
+`;
